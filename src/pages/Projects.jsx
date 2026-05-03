5380985562270ad71099f3c71729f354425b7832
Hasan Saeed <hasanch.0170@gmail.com>
2026-05-03 17:09:14 +0500

    ProjectsCTA

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ProjectsHero from '../components/core/Projects/ProjectsHero';
 import ProjectsGrid from '../components/core/Projects/ProjectsGrid';
-
+import ProjectsCTA from '../components/core/Projects/ProjectsCTA';
 
 const Projects = () => {
   return (
     <>
       <ProjectsHero />
       <ProjectsGrid />
+      <ProjectsCTA />
     </>
   );
 };
