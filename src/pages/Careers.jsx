241b7377318f2d240b1b4d341b81e5f80ff80841
Hasan Saeed <hasanch.0170@gmail.com>
2026-05-03 19:09:20 +0500

    CareerHero

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -1,5 +1,11 @@
-const Careers = () => {
-  return <h1>Careers Page</h1>;
-};
+import React from 'react';
+import CareerHero from '../components/core/Career/CareerHero';
 
+const Careers = () => {
+  return (
+    <>
+      <CareerHero />
+    </>
+  );
+};
 export default Careers;
