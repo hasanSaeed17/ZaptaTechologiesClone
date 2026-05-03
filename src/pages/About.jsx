1bedf0b42fea46e2ab0dfe350446f4a1da39ca64
Hasan Saeed <hasanch.0170@gmail.com>
2026-05-03 18:29:22 +0500

    AboutCTA

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import AboutHero from '../components/core/About/AboutHero';
+import AboutCTA from '../components/core/About/AboutCTA';
 
 const About = () => {
   return (
     <>
       <AboutHero />
+
+      <AboutCTA />
     </>
   );
 };
